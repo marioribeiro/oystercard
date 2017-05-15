2921a1a3974aae7318143abe4506b06fc60a98e8
Mário Ribeiro <mail@marioribeiro.com>
2017-05-15 18:46:30 +0100

    Start coding the Oystercard class
    - Class responds to balance
    - Initial balance is set to 0

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,15 @@
+require 'oystercard'
+
 describe Oystercard do
+
+  subject(:card) { Oystercard.new }
+
+  it { is_expected.to respond_to(:balance) }
+
+  describe '#balance' do
+    it 'returns the balance' do
+      expect(card.balance).to eq 0
+    end
+  end
+ 
 end
