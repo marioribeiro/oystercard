9f398f088ba025970f75e4f2e964e180c47bb5f2
Mário Ribeiro <mail@marioribeiro.com>
2017-05-15 18:53:53 +0100

    Add check to top_up if it a the top up make the card reach the maximum balance allowed

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -15,10 +15,17 @@ describe Oystercard do
 
   describe '#top_up' do
     it { is_expected.to respond_to(:top_up).with(1).argument }
+
     it 'can top up the balance' do
       expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
     end
 
-end
+    it 'raises an error if the maximum balance is exceeded' do
+      maximum_balance = Oystercard::MAXIMUM_BALANCE
+      subject.top_up(maximum_balance)
+      expect{ subject.top_up(1) }.to raise_error 'Error: Maximum balance exceeded'
+    end
+    
+  end
  
 end
