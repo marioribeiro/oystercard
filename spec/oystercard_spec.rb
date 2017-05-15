c6bb5fb15c0ae97f82fae90310f59688e5709edd
Mário Ribeiro <mail@marioribeiro.com>
2017-05-15 18:49:47 +0100

    Add top_up method that accepts an amount as argument and increases balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,11 +5,20 @@ describe Oystercard do
   subject(:card) { Oystercard.new }
 
   it { is_expected.to respond_to(:balance) }
+  it { is_expected.to respond_to(:top_up).with(1).argument }
 
   describe '#balance' do
     it 'returns the balance' do
       expect(card.balance).to eq 0
     end
   end
+
+  describe '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
+    it 'can top up the balance' do
+      expect{ subject.top_up 5 }.to change{ subject.balance }.by 5
+    end
+
+end
  
 end
