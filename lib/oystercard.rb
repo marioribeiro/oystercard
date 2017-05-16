83500a42898a3c8a3149a61c5e6fa34e8acc9b8f
charliefea <charlotte.feather@yahoo.com>
2017-05-16 16:31:36 +0100

    Refactored and removed entry and exit station instant variables

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -4,11 +4,12 @@ class Oystercard
   MINIMUM_BALANCE = 1
   MINIMUM_FARE = 1
 
-  attr_reader :balance, :in_journey, :entry_station
+  attr_reader :balance, :journey_history
 
   def initialize
     @balance = 0
-    @entry_station = nil
+    @journey_history = []
+
   end
 
   def top_up(amount)
@@ -19,10 +20,13 @@ class Oystercard
   def touch_in(station)
     fail "Error: You need to top up" if @balance < MINIMUM_BALANCE
     @entry_station = station
+    @journey_history.push(entry_station: station, exit_station: nil)
+    @exit_station = nil
   end
 
-  def touch_out
+  def touch_out(station)
     deduct(MINIMUM_FARE)
+    @journey_history[-1][:exit_station] = station
     @entry_station = nil
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,16 +3,15 @@ require 'oystercard'
 describe Oystercard do
 
   subject(:card) { Oystercard.new }
-  let(:station) { double(:station) }
+  let(:entry_station) { double(:entry_station) }
+  let(:exit_station) { double(:exit_station) }
+  let(:journey){ {entry_station: entry_station, exit_station: exit_station} }
 
-  it { is_expected.to respond_to(:balance) }
-  it { is_expected.to respond_to(:top_up).with(1).argument }
-  it { is_expected.to respond_to(:touch_in).with(1).argument }
-  it { is_expected.to respond_to(:touch_out) }
-  it { is_expected.to respond_to(:in_journey) }
-  it { is_expected.to respond_to(:in_journey?)}
-  it { is_expected.to respond_to(:entry_station)}
-
+  describe 'default' do
+    it 'expect an empty list of journeys on initialization' do
+      expect(card.journey_history).to be_empty
+    end
+  end
 
   describe '#balance' do
     it 'returns the balance' do
@@ -21,58 +20,66 @@ describe Oystercard do
   end
 
   describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
 
     it 'can top up the balance' do
-      expect{ subject.top_up Oystercard::MAXIMUM_BALANCE }.to change{ subject.balance }.by Oystercard::MAXIMUM_BALANCE
+      expect{ card.top_up Oystercard::MAXIMUM_BALANCE }.to change{ card.balance }.by Oystercard::MAXIMUM_BALANCE
     end
 
     it 'raises an error if the maximum balance is exceeded' do
       maximum_balance = Oystercard::MAXIMUM_BALANCE
-      subject.top_up(maximum_balance)
-      expect{ subject.top_up(1) }.to raise_error "Error: Maximum balance of #{maximum_balance} exceeded"
+      card.top_up(maximum_balance)
+      expect{ card.top_up(1) }.to raise_error "Error: Maximum balance of #{maximum_balance} exceeded"
     end
   end
 
   describe '#touch_in' do
     it 'sets instance variable "in_journey" to true' do
       card.top_up(Oystercard::MAXIMUM_BALANCE)
-      card.touch_in(station)
+      card.touch_in(entry_station)
       expect(card).to be_in_journey
     end
 
     it 'prevents touching in if balance is below the minimum balance' do
-      expect{ card.touch_in(station) }.to raise_error "Error: You need to top up"
+      expect{ card.touch_in(entry_station) }.to raise_error "Error: You need to top up"
     end
 
     it 'sets the entry station' do
       card.top_up(Oystercard::MAXIMUM_BALANCE)
-      card.touch_in(station)
-      expect(card.entry_station).to eq station
+      card.touch_in(entry_station)
+      expect(card.journey_history[-1][:entry_station]).to eq entry_station
     end
   end
 
   describe '#touch_out' do
+
+    before do
+      card.top_up(Oystercard::MINIMUM_BALANCE)
+      card.touch_in(entry_station)
+    end
     it 'sets instance variable "in_journey to false' do
-      card.touch_out
+      card.touch_out(exit_station)
       expect(card).to_not be_in_journey
     end
 
     it 'deducts minimum fare' do
-      card.top_up(Oystercard::MINIMUM_BALANCE)
-      expect{ card.touch_out }.to change{ card.balance }.by -Oystercard::MINIMUM_FARE  
+      expect{ card.touch_out(exit_station) }.to change{ card.balance }.by -Oystercard::MINIMUM_FARE
     end
 
-    it 'set entry station to nil' do
-      card.top_up(Oystercard::MINIMUM_BALANCE)
-      card.touch_in(station)
-      card.touch_out
-      expect(card.entry_station).to eq nil
+
+    it 'sets exit station' do
+      card.touch_out(exit_station)
+      expect(card.journey_history[-1][:exit_station]).to eq exit_station
     end
 
+    it 'created a new entry in the journey_history hash including the entry and exit stations' do
+
+      card.touch_out(exit_station)
+      expect(card.journey_history).to include journey
+    end
   end
 
   describe '#in_journey?' do
+
     it 'returns true or false' do
       expect(card.in_journey?).to be(true).or be(false)
     end
