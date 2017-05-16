264f21845a2e5f7588b4e9d56afbf7a39b3ad82d
Mário Ribeiro <mail@marioribeiro.com>
2017-05-16 18:31:10 +0100

    Refactor in_journey? method

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -9,7 +9,6 @@ class Oystercard
   def initialize
     @balance = 0
     @journey_history = []
-
   end
 
   def top_up(amount)
@@ -21,17 +20,16 @@ class Oystercard
     fail "Error: You need to top up" if @balance < MINIMUM_BALANCE
     @entry_station = station
     @journey_history.push(entry_station: station, exit_station: nil)
-    @exit_station = nil
   end
 
   def touch_out(station)
     deduct(MINIMUM_FARE)
     @journey_history[-1][:exit_station] = station
-    @entry_station = nil
   end
 
   def in_journey?
-    !!@entry_station
+    return false if journey_history.empty?
+    @journey_history.last[:exit_station].nil?
   end
 
   private
@@ -41,3 +39,5 @@ class Oystercard
   end
 
 end
+
+
